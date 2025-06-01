9fd2503d92c4de1d5155897bb9db071f06549dcb
vvina-ten <ninieva866@gmail.com>
2025-06-01 15:04:13 -0400

    step2 finish upload and moving zombie with health
    yeah,mooooooving

---
+++ A2P2_2/A2P2_1/Game1.cs
@@ -0,0 +1,78 @@
+﻿using A2P2_1.Content;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace A2P2_1
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+
+        private Zombie _zombie;
+        
+
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            
+            _zombie = new Zombie(3,Content.Load<Texture2D>("zombie"), Content.Load<SpriteFont>("gamefont"));
+            
+            _zombie.SetScale(1f);
+          
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            
+            _zombie.Update();
+            
+
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch .Begin();
+
+
+
+            _zombie.Draw(_spriteBatch);
+
+
+
+
+            _spriteBatch.End();
+
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ A2P2_2/A2P2_1/Zombie.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace A2P2_1.Content
+{
+    public class Zombie
+    {
+
+        private Texture2D zombietex;
+
+        private int health;
+        private int damage;
+
+        private float scale;
+        private float movespeed;
+        private SpriteFont gamefont;
+        private Vector2 position;
+
+        public Zombie(int health, Texture2D zombietex, SpriteFont gamefont)
+        {
+
+            this.health = health;
+            this.zombietex = zombietex;
+            this.gamefont = gamefont;
+            this.position = new Vector2(500,50);
+            this.scale = 0.5f;
+            this.movespeed = 2f;
+
+        }
+
+        public int GetHealth()
+        {
+
+            return this.health;
+
+        }
+
+
+        public void SetScale(float newScale)
+        {
+
+            this.scale = newScale;
+
+        }
+
+        public void Update() 
+        {
+
+            position.X -= movespeed;
+
+        
+        }
+
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+           
+            Rectangle destRect = new Rectangle
+             (
+                (int)position.X,(int)position.Y,130,180);
+
+            spriteBatch.Draw(zombietex, destRect, Color.White);
+
+
+
+            string healthbar = "Health: " + health.ToString();
+
+            Vector2 position2 = new Vector2(position.X, position.Y - 20);
+
+            spriteBatch.DrawString(gamefont, healthbar, position2, Color.Blue);
+
+        }
+
+
+
+
+
+
+
+    }
+}
