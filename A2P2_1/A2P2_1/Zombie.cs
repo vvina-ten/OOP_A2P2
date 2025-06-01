84ab842ee759520e6ea52d7754af5b1b84b3f474
vvina-ten <ninieva866@gmail.com>
2025-06-01 03:21:14 -0400

    A2P2 step1 upload  2d and health
    it should be picture and word but it didn't...

---
+++ A2P2_1/A2P2_1/Zombie.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace A2P2_1.Content
+{
+    public class Zombie
+    {
+
+        private Texture2D zombietex;
+
+        private int health;
+        private int damage;
+
+        private float scale;
+
+        private SpriteFont gamefont;
+        private Vector2 position;
+
+        public Zombie(int health, Texture2D zombietex, SpriteFont gamefont)
+        {
+
+            this.health = health;
+            this.zombietex = zombietex;
+            this.gamefont = gamefont;
+            this.position = new Vector2(100,50);
+            this.scale = 0.5f;
+
+        }
+
+        public int GetHealth()
+        {
+
+            return this.health;
+
+        }
+
+
+        public void SetScale(float newScale)
+        {
+
+            this.scale = newScale;
+
+        }
+   
+
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+           
+            Rectangle destRect = new Rectangle
+             (
+                (int)position.X,(int)position.Y,50,50);
+
+
+            string healthbar = "Health: " + health.ToString();
+
+            Vector2 position2 = new Vector2(position.X, position.Y - 20);
+
+            spriteBatch.DrawString(gamefont, healthbar, position2, Color.Blue);
+
+        }
+
+
+
+
+
+
+
+    }
+}
